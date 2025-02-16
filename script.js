c0d8911a971023280a328145aa708ca86018621a
zNickGT <thenick12345678901@gmail.com>
2025-02-15 21:14:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("Circle").addEventListener("click", function() {
+    alert("Hello! This is your first interactive website.");
+});
+
+document.getElementById("Square").addEventListener("click", function() {
+    alert("Hello! This is your first interactive website.");
+});
+
+document.getElementById("Triangle").addEventListener("click", function() {
+    alert("Hello! This is your first interactive website.");
+});
